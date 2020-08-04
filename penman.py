71ecbedd212e3720af766fbd302d4d26bd0f99e5
Andy Wickert <andy@northernwidget.com>
2020-08-04 13:24:52 -0500

    start: longwave radiation estimate

---
+++ penman.py
@@ -0,0 +1,43 @@
+# Using TerraClimate
+
+
+solar_constant = 1360.8 # W/m^2
+stefan_boltzmann_constant = 5.67E-8
+
+def _computeClearSkySolarRadiation(elevation, julian_day, latitude):
+
+    # Step 1: top-of-atmosphere ("extraterrestrial") radiation
+    # Change 365 to number of days in year?
+    inverse_relative_earth_sun_distance = 1 + 0.033 * np.cos(2*np.pi/365. * julian_day )
+    solar_declination = 0.409 * np.sin( 2*np.pi/365. * julian_day - 1.39 )
+    
+    sunset_hour_angle = np.arccos( -np.tan(np.pi/180. * latitude) 
+                                   * np.tan(solar_declination) )
+    
+    extraterrestrial_radiation = solar_constant * inverse_relative_earth_sun_distance \
+                                 * ( sunset_hour_angle
+                                     * np.sin(np.pi/180. * latitude)
+                                     * np.sin(solar_declination)
+                                     + np.cos(np.pi/180. * latitude)
+                                     * np.cos(solar_declination)
+                                     * np.sin(sunset_hour_angle) )
+
+    clear_sky_solar_radiation = (0.75 + 2E-5 * elevation) * extraterrestrial_radiation
+    return clear_sky_solar_radiation
+
+def computeNetLongwaveRadiation(elevation, julian_day, latitude,
+                                Tmax_degC, Tmin_degC, vapor_pressure,
+                                incoming_solar_radiation,
+                                clear_sky_solar_radiation):
+    """
+    Following Zotarelli et al., 2010
+    If this is for the land surface, it may need some modification to be
+    appropriate for lakes and their heating/cooling
+    """
+    Rnl = stefan_boltzmann_constant \
+              * ( (Tmax_degC + 273.16)**4 + (Tmin_degC + 273.16)**4 ) / 2. \
+              * (.34 - .14 * vapor_pressure**.5) \
+              * (1.35*incoming_solar_radiation/clear_sky_solar_radiation - 0.35)
+    return Rnl
+    
+    
